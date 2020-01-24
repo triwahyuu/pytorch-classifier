09e0f86df8d2fbe088ccb61f06a760a72307616d
triwahyuu <triwahyu34@gmail.com>
2020-01-24 21:26:42 +0700

    adding utils

--- train.py
+++ train.py
@@ -3,21 +3,19 @@ from __future__ import print_function, division
 import multiprocessing
 multiprocessing.set_start_method('spawn', True)
 
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torchvision
-from torchvision import datasets, models, transforms
+from torchvision import models, datasets, transforms
 from torch.utils.tensorboard import SummaryWriter
 
-import matplotlib.pyplot as plt
-import tqdm
+from utils import prepare_model
 
 import os
 import time
 import argparse
 import datetime
+import tqdm
 
 
 def prepare_dataloaders(datapath='dataset/', img_size=224, batch_size=4):
@@ -40,57 +38,13 @@ def prepare_dataloaders(datapath='dataset/', img_size=224, batch_size=4):
 
     image_datasets = {x: datasets.ImageFolder(os.path.join(datapath, x), data_transforms[x]) \
                       for x in ['train', 'val']}
-    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size,
-                   shuffle=True, num_workers=4, pin_memory=True) for x in ['train', 'val']}
+    dataloaders = {'train': torch.utils.data.DataLoader(image_datasets['train'], batch_size=batch_size,
+                        shuffle=True, num_workers=4, pin_memory=True),
+                    'val': torch.utils.data.DataLoader(image_datasets['val'], batch_size=1,
+                        shuffle=False, num_workers=1, pin_memory=True)}
 
     return dataloaders
 
-
-def prepare_model(arch, n_class, freeze_layer=True):
-    model = getattr(models, arch)(pretrained=True)
-
-    if freeze_layer:
-        for param in model.parameters():
-            param.requires_grad = False
-    
-    optimizer = None
-    if "resnet" in arch:
-        num_features = model.fc.in_features
-        model.fc = nn.Linear(num_features, n_class)
-        model.to(device)
-
-        optimizer = optim.SGD(model.fc.parameters(), lr=0.001, momentum=0.9)
-    elif "vgg" in arch:
-        num_features = model.classifier[0].in_features
-        model.classifier = nn.Sequential(
-            nn.Linear(in_features),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, num_classes),
-        )
-        model.to(device)
-
-        optimizer = optim.SGD(model.classifier.parameters(), lr=0.001, momentum=0.9)
-
-    ## loss function
-    criterion = nn.CrossEntropyLoss().to(device)
-    return model, criterion, optimizer
-
-def visualise(inp, title=None):
-    if type(inp) == torch.Tensor:
-        img = inp.numpy().transpose((1, 2, 0))
-        mean = np.array([0.485, 0.456, 0.406])
-        std = np.array([0.229, 0.224, 0.225])
-        img = std * img + mean
-        img = np.clip(img, 0, 1)
-    
-    plt.imshow(img)
-    if title is not None:
-        plt.title(title)
-    plt.show()
 
 def train_model(model, arch, dataloaders, criterion, optimizer, 
         scheduler=None, num_epochs=25, output_path='result/', start_epoch=0):
@@ -103,10 +57,10 @@ def train_model(model, arch, dataloaders, criterion, optimizer,
 
     writer = SummaryWriter(os.path.join(output_path, "tb"))
     with open(os.path.join(output_path, "log.txt"), 'w') as f:
-        metrics = "phase, epoch, loss, correct"
+        metrics = "phase, epoch, loss, accuracy"
         f.write(metrics + '\n')
 
-    for epoch in tqdm.trange(start_epoch, num_epochs, total=num_epochs, desc="epoch ", ncols=80):
+    for epoch in tqdm.trange(start_epoch, num_epochs, desc="epoch ", ncols=80):
         # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
             if phase == 'train':
@@ -182,18 +136,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-a", "--arch", metavar="arch", default="resnet101", choices=model_choices,
-        help="model architecture: " + " | ".join(model_choices) +  "(default: resnet101)"
+        help="model architecture: " + " | ".join(model_choices) +  " (default: resnet101)"
     )
-    parser.add_argument(
-        '--datapath', default='dataset/', help='suction grasp dataset path',
-    )
-    parser.add_argument(
-        '--output-path', default='', help='training result path',
-    )
+    parser.add_argument('--datapath', default='dataset/', help='suction grasp dataset path')
+    parser.add_argument('--output-path', default='', help='training result path')
     parser.add_argument("--resume", default="", type=str, help="checkpoint path to resume training")
-    parser.add_argument("--epochs", type=int, default=50, help="number of epochs for training")
-    parser.add_argument("--batch_size", type=int, default=4, help="size of each image batch")
-    parser.add_argument("--img_size", type=int, default=224, help="size of each image dimension")
+    parser.add_argument("--epochs", type=int, default=100, help="number of epochs of training")
+    parser.add_argument("--batch-size", type=int, default=4, help="batch size for training")
+    parser.add_argument("--img-size", type=int, default=224, help="image dataset size in training")
+    parser.add_argument('--use-scheduler', action='store_true', help='use lr scheduler')
     args = parser.parse_args()
 
     print(f"Training {args.arch}")
@@ -219,7 +170,10 @@ if __name__ == "__main__":
         model.load_state_dict(checkpoint['model_state_dict'], strict=True)
         optimizer.load_state_dict(checkpoint['optim_state_dict'])
 
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+    if args.use_scheduler:
+        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+    else:
+        scheduler = None
 
     train_model(model, args.arch, dataloaders, criterion, optimizer, scheduler=scheduler, 
         num_epochs=args.epochs, output_path=os.path.join("result", args.arch, now), start_epoch=start_epoch)
---
+++ utils.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, models, transforms
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def visualise(inp, title=None):
+    if type(inp) == torch.Tensor:
+        img = inp.numpy().transpose((1, 2, 0))
+        mean = np.array([0.485, 0.456, 0.406])
+        std = np.array([0.229, 0.224, 0.225])
+        img = std * img + mean
+        img = np.clip(img, 0, 1)
+    
+    plt.imshow(img)
+    if title is not None:
+        plt.title(title)
+    plt.show()
+
+
+def prepare_model(arch, n_class, freeze_layer=True):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    model = getattr(models, arch)(pretrained=True)
+    if freeze_layer:
+        for param in model.parameters():
+            param.requires_grad = False
+    
+    optimizer = None
+    if "resnet" in arch:
+        num_features = model.fc.in_features
+        model.fc = nn.Linear(num_features, n_class)
+        model.to(device)
+
+        optimizer = optim.SGD(model.fc.parameters(), lr=0.001, momentum=0.9)
+    elif "vgg" in arch:
+        num_features = model.classifier[0].in_features
+        model.classifier = nn.Sequential(
+            nn.Linear(num_features, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, n_class),
+        )
+        model.to(device)
+
+        optimizer = optim.SGD(model.classifier.parameters(), lr=0.001, momentum=0.9)
+
+    ## loss function
+    criterion = nn.CrossEntropyLoss().to(device)
+    return model, criterion, optimizer
